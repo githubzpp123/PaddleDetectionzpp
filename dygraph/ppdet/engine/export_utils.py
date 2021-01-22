bb99cfb88d223439dd9fa0c49976b6e85693e511
Guanghua Yu <742925032@qq.com>
2021-01-22 11:57:36 +0800

    Revert "[Dygraph] Fix SSD-MobileNet  (#2109)"
    This reverts commit 5d7f09edce44223ea97cc0405d5190c55c17a110.

--- dygraph/ppdet/engine/export_utils.py
+++ dygraph/ppdet/engine/export_utils.py
@@ -28,13 +28,13 @@ logger = setup_logger(__name__)
 # Global dictionary
 TRT_MIN_SUBGRAPH = {
     'YOLO': 3,
-    'SSD': 40,
+    'SSD': 3,
     'RCNN': 40,
     'RetinaNet': 40,
     'EfficientDet': 40,
     'Face': 3,
-    'TTFNet': 40,
-    'FCOS': 40,
+    'TTFNet': 3,
+    'FCOS': 3,
     'SOLOv2': 60,
 }
 
